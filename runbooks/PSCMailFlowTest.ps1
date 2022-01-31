c6b6b15914111c512a08ef5676d2924d8b67b579
asallahi <84030478+asallahi@users.noreply.github.com>
2022-01-31 16:38:23 -0500

    Update PSCMailFlowTest.ps1

--- runbooks/PSCMailFlowTest.ps1
+++ runbooks/PSCMailFlowTest.ps1
@@ -20,6 +20,7 @@
 Import-Module MSAL.PS
 Import-Module OMSIngestionAPI
 
+$tenant ='PSC'
 #--- Get AD Application info from variables ---#
 $clientId = Get-AutomationVariable -Name 'PSCClientIDMailflow'
 $tenantId = Get-AutomationVariable -Name 'PSCTenantID'
@@ -87,6 +88,7 @@ $omsjson = @"
 [{  "Computer": "$ENV:COMPUTERNAME",
     "TestType": "mailflow",
     "TestID": "$TestID",
+    "Tenant": "$tenant",
     "TransactionType": "sendmessage",
     "TransactionResult": "$SendStatus",
     "TransactionTime": $SendTime,
@@ -140,6 +142,7 @@ $omsjson = @"
 [{   "Computer": "$ENV:COMPUTERNAME",
     "TestType": "mailflow",
     "TestID": "$TestID",
+    "Tenant": "$tenant",
     "TransactionType": "searchformessage",
     "TransactionResult": "$ReceiveStatus",
     "TransactionTime": $ReceiveTime,
