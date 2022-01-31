96a97693d438affe4336fad0e21145c876f1c8b0
asallahi <84030478+asallahi@users.noreply.github.com>
2022-01-31 16:31:51 -0500

    Update FINMailFlowTest.ps1

--- runbooks/FINMailFlowTest.ps1
+++ runbooks/FINMailFlowTest.ps1
@@ -88,6 +88,7 @@ $omsjson = @"
 [{  "Computer": "$ENV:COMPUTERNAME",
     "TestType": "mailflow",
     "TestID": "$TestID",
+    "Tenant": "$tenant",
     "TransactionType": "sendmessage",
     "TransactionResult": "$SendStatus",
     "TransactionTime": $SendTime,
