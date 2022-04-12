265943da99d240475f7e8a9c720dc3821a2be225
kc-free <82967162+kc-free@users.noreply.github.com>
2022-04-12 09:18:58 -0400

    Update PSCMailFlowTest.ps1

--- runbooks/PSCMailFlowTest.ps1
+++ runbooks/PSCMailFlowTest.ps1
@@ -20,6 +20,7 @@
 Import-Module MSAL.PS
 Import-Module OMSIngestionAPI
 
+$jobid = $PSPrivateMetadata.JobId.Guid
 $tenant ='PSC'
 #--- Get AD Application info from variables ---#
 $clientId = Get-AutomationVariable -Name 'PSCClientIDMailflow'
@@ -92,7 +93,8 @@ $omsjson = @"
     "TransactionType": "sendmessage",
     "TransactionResult": "$SendStatus",
     "TransactionTime": $SendTime,
-    "TransactionMessage": "$TransMsg"
+    "TransactionMessage": "$TransMsg",
+    "JobID": "$jobid"
 }]
 "@
 
@@ -146,7 +148,8 @@ $omsjson = @"
     "TransactionType": "searchformessage",
     "TransactionResult": "$ReceiveStatus",
     "TransactionTime": $ReceiveTime,
-    "TransactionMessage": "$TransMsg"
+    "TransactionMessage": "$TransMsg",
+    "JobID": "$jobid"
 }]
 "@
 
